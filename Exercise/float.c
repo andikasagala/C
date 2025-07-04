d831bf0ba10102c6d297d22d7e9832c929288db1
andikasagala <andikagaming1986@gmail.com>
2025-07-05 00:08:00 +0700

    Create float.c

---
+++ Exercise/float.c
@@ -0,0 +1,21 @@
+/*
+Profile,
+Coder Name: Andika S
+GitHUb: https://github.com/andikasagala
+LinkedIn: https://www.linkedin.com/in/andika-s99
+*/
+
+
+#include <stdio.h>
+
+// float
+
+int main () {
+
+	float Var1 = 3.14; // circle value by use float
+
+	printf("Circle is %f  \n", Var1); // print out the circle value
+
+
+	return 0;
+}
