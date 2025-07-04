4e22739753faf64ec34d5f95ecfdfd307ff52805
andikasagala <andikagaming1986@gmail.com>
2025-07-04 22:59:50 +0700

    Create hello world.c

---
+++ Exercise/hello world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//Profile,
+// Name: Andika S
+// GitHUb: https://github.com/andikasagala
+// LinkedIn: https://www.linkedin.com/in/andika-s99
+
+
+int main () {
+
+	printf("Hello world ! \n");
+	
+	return 0;
+}
