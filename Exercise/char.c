aeac90be9d4835ef22c8c6b3ae2399e4ae7f093f
andikasagala <andikagaming1986@gmail.com>
2025-07-05 00:59:29 +0700

    Create char.c

---
+++ Exercise/char.c
@@ -0,0 +1,27 @@
+/*
+Profile,
+Coder Name: Andika S
+GitHUb: https://github.com/andikasagala
+LinkedIn: https://www.linkedin.com/in/andika-s99
+*/
+
+
+#include <stdio.h>
+
+// char
+
+int main () {
+ 
+	char Char1 = 'A'; // declare the Char1 
+	char Char2, Char3; 
+	Char2 = 'B';
+	Char3 = 'C';
+
+	printf("Char1 is %c \n",Char1); // print out the Char1
+	printf("Char2 is %c \n", Char2); // print out the Char2
+	printf("Char3 is %c \n", Char3); // print out the Char3
+
+
+
+	return 0;
+}
