02354278596d46b39f6aae803c66be1a87f5a407
andikasagala <andikagaming1986@gmail.com>
2025-07-05 04:49:38 +0700

    Create multiple printf.c

---
+++ Exercise/multiple printf.c
@@ -0,0 +1,22 @@
+/*
+Profile,
+Coder Name: Andika S
+GitHUb: https://github.com/andikasagala
+LinkedIn: https://www.linkedin.com/in/andika-s99
+*/
+
+
+#include <stdio.h>
+
+// multiple printf
+
+int main () {
+ 
+	int A = 2; // declare the variable A
+
+
+	printf("the A value is %d \n ", A); // print out  the variable A
+	printf("The A+1 is %d . The A+2 is %d . The A+3 is %d \n ", A + 1, A + 2, A + 3); // print out the multiple variable
+
+	return 0;
+}
