f8834f4d4471b60aa1d2add7df9a345673a52b0d
andikasagala <andikagaming1986@gmail.com>
2025-07-04 23:42:51 +0700

    Update hello world.c

--- Exercise/hello world.c
+++ Exercise/hello world.c
@@ -1,13 +1,14 @@
-// Profile,
-// Coder Name: Andika S
-// GitHUb: https://github.com/andikasagala
-// LinkedIn: https://www.linkedin.com/in/andika-s99
-
+/*
+Profile,
+Coder Name: Andika S
+GitHUb: https://github.com/andikasagala
+LinkedIn: https://www.linkedin.com/in/andika-s99
+*/
 
 
 #include <stdio.h>
 
-
+// Hello world codes
 
 int main () {
 
