725ab56b16a3c698e09c5c996678cdf86ae944fd
andikasagala <andikagaming1986@gmail.com>
2025-07-05 01:05:16 +0700

    Update char.c

--- Exercise/char.c
+++ Exercise/char.c
@@ -16,11 +16,12 @@ int main () {
 	char Char2, Char3; 
 	Char2 = 'B';
 	Char3 = 'C';
+	char Char4 = 101; // reference: https://en.wikipedia.org/wiki/ASCII
 
 	printf("Char1 is %c \n",Char1); // print out the Char1
 	printf("Char2 is %c \n", Char2); // print out the Char2
 	printf("Char3 is %c \n", Char3); // print out the Char3
-
+	printf("Char4 is %c \n", Char4); // print out the Char4
 
 
 	return 0;
