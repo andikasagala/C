bf95e6baa2292373f9b1697f468ff03842c11bfc
andikasagala <andikagaming1986@gmail.com>
2025-07-05 00:15:32 +0700

    Create double.c

---
+++ Exercise/double.c
@@ -0,0 +1,25 @@
+/*
+Profile,
+Coder Name: Andika S
+GitHUb: https://github.com/andikasagala
+LinkedIn: https://www.linkedin.com/in/andika-s99
+*/
+
+
+#include <stdio.h>
+
+// double
+
+int main () {
+
+	float Var1 = 3.1476845467; // float
+	double Var2 = 3.1476845467; // double
+
+
+	printf("My float is %f  \n", Var1); // print out the float value
+	printf("My double is %lf  \n", Var2); // print out the double value
+
+
+
+	return 0;
+}
