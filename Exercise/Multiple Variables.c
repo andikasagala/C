97defb01677971b4931e9bc6aed7d9a3c8e7dd8a
andikasagala <andikagaming1986@gmail.com>
2025-07-04 23:51:06 +0700

    update

---
+++ Exercise/Multiple Variables.c
@@ -0,0 +1,28 @@
+/*
+Profile,
+Coder Name: Andika S
+GitHUb: https://github.com/andikasagala
+LinkedIn: https://www.linkedin.com/in/andika-s99
+*/
+
+
+#include <stdio.h>
+
+// Multiple Variables
+
+int main () {
+
+	int Var1; // declare the variable 1 by using INT
+	Var1 = 1; // declare the variable value
+	int Var2; // declare the variable 2 by using INT
+	Var2 = 2; // declare the variable value
+	int Var3; // declare the variable 3 by using INT
+	Var3 = 3; // declare the variable value
+
+	printf("Var1 value is %d \n", Var1); // print out the Var1 value
+	printf("Var2 value is %d \n", Var2); // print out the Var2 value
+	printf("Var3 value is %d \n", Var3); // print out the Var3 value
+
+	
+	return 0;
+}
---
+++ Exercise/Variable.c
@@ -0,0 +1,23 @@
+/*
+Profile,
+Coder Name: Andika S
+GitHUb: https://github.com/andikasagala
+LinkedIn: https://www.linkedin.com/in/andika-s99
+*/
+
+
+#include <stdio.h>
+
+// Variable
+
+int main () {
+
+	int Var1; // declare the variable 1 by using INT
+	Var1 = 1; // declare the variable value
+	
+
+	printf("Var1 value is %d \n", Var1); // print out the Var1 value
+	
+	
+	return 0;
+}
